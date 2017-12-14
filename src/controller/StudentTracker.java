bf7b5a48e35e1590708902f296c873f75d87b9bd
wavis421 <wendy.avis@jointheleague.org>
2017-12-13 22:05:20 -0800

    Import from last 7 days instead of 4.

--- src/controller/StudentTracker.java
+++ src/controller/StudentTracker.java
@@ -5,7 +5,7 @@ import java.util.prefs.Preferences;
 
 import org.joda.time.DateTime;
 
-import model.ActivityEventModel;
+import model.AttendanceEventModel;
 import model.LogDataModel;
 import model.MySqlDatabase;
 import model.ScheduleModel;
@@ -24,7 +24,7 @@ public class StudentTracker {
 
 	public StudentTracker() {
 		// Import data starting 4 days ago
-		DateTime startDate = new DateTime().minusDays(4);
+		DateTime startDate = new DateTime().minusDays(7);
 		String startDateString = startDate.toString().substring(0, 10);
 
 		// Retrieve tokens and passwords
@@ -43,7 +43,7 @@ public class StudentTracker {
 		// Import all the League databases
 		Pike13Api pike13Api = new Pike13Api(sqlDb, pike13Token);
 		importStudentsFromPike13(pike13Api);
-		importActivitiesFromPike13(startDateString, pike13Api);
+		importAttendanceFromPike13(startDateString, pike13Api);
 		importScheduleFromPike13(pike13Api);
 
 		GithubApi githubApi = new GithubApi(sqlDb, githubToken);
@@ -66,16 +66,16 @@ public class StudentTracker {
 		sqlDb.insertLogData(LogDataModel.STUDENT_IMPORT_COMPLETE, new StudentNameModel("", "", false), 0, " ***");
 	}
 
-	public void importActivitiesFromPike13(String startDate, Pike13Api pike13Api) {
+	public void importAttendanceFromPike13(String startDate, Pike13Api pike13Api) {
 		sqlDb.insertLogData(LogDataModel.STARTING_ATTENDANCE_IMPORT, new StudentNameModel("", "", false), 0,
 				" starting after " + startDate + " ***");
 
 		// Get data from Pike13
-		ArrayList<ActivityEventModel> eventList = pike13Api.getEnrollment(startDate);
+		ArrayList<AttendanceEventModel> eventList = pike13Api.getEnrollment(startDate);
 
 		// Update changes in database
 		if (eventList.size() > 0)
-			sqlDb.importActivities(eventList);
+			sqlDb.importAttendance(eventList);
 
 		sqlDb.insertLogData(LogDataModel.ATTENDANCE_IMPORT_COMPLETE, new StudentNameModel("", "", false), 0, " ***");
 	}
